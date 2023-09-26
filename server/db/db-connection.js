a18c1647decc2adf9b7f4fc93b5ab38613c52d6d
Janet Jiang <janetjiang1103@gmail.com>
2023-09-26 21:24:59 +0800

    updating server.js to create put and post routes

--- server/db/db-connection.js
+++ server/db/db-connection.js
@@ -1,6 +1,8 @@
 const { Pool } = require('pg');
 const db = new Pool({
-    connectionString: process.env.DB_URI
-  });
+  connectionString: process.env.DB_URI,
+  ssl: (process.env.DATABASE_SSL != "false") 
+  }
+);
 
-  module.exports = db;
+module.exports = db;
--- server/server.js
+++ server/server.js
@@ -12,31 +12,32 @@ app.use(express.json());
 
 // creates an endpoint for the route "/""
 app.get('/', (req, res) => {
-    res.json({ message: 'Hola, from My template ExpressJS with React-Vite' });
+    res.json({ message: 'Welcome to my contact list API' });
 });
 
-// create the get request for students in the endpoint '/api/students'
-app.get('/api/students', async (req, res) => {
+// create the get request for contact in the endpoint '/api/contactlist'
+app.get('/api/contactlist', async (req, res) => {
     try {
-        const { rows: students } = await db.query('SELECT * FROM students');
-        res.send(students);
+        const { rows: contact } = await db.query('SELECT * FROM contact');
+        res.send(contact);
     } catch (e) {
         return res.status(400).json({ e });
     }
 });
 
 // create the POST request
-app.post('/api/students', async (req, res) => {
+app.post('/api/contactlist', async (req, res) => {
     try {
-        const newStudent = {
-            firstname: req.body.firstname,
-            lastname: req.body.lastname,
-            iscurrent: req.body.iscurrent
+        const newContact = {
+            name: req.body.name,
+            email: req.body.email,
+            phone: req.body.phone,
+            notes: req.body.notes,
         };
         //console.log([newStudent.firstname, newStudent.lastname, newStudent.iscurrent]);
         const result = await db.query(
-            'INSERT INTO students(firstname, lastname, is_current) VALUES($1, $2, $3) RETURNING *',
-            [newStudent.firstname, newStudent.lastname, newStudent.iscurrent],
+            'INSERT INTO contact(name, email, phone, notes) VALUES($1, $2, $3, $4) RETURNING *',
+            [newContact.name, newContact.email, newContact.phone, newContact.notes],
         );
         console.log(result.rows[0]);
         res.json(result.rows[0]);
@@ -48,31 +49,31 @@ app.post('/api/students', async (req, res) => {
 
 });
 
-// delete request for students
-app.delete('/api/students/:studentId', async (req, res) => {
-    try {
-        const studentId = req.params.studentId;
-        await db.query('DELETE FROM students WHERE id=$1', [studentId]);
-        console.log("From the delete request-url", studentId);
-        res.status(200).end();
-    } catch (e) {
-        console.log(e);
-        return res.status(400).json({ e });
+// // delete request for students
+// app.delete('/api/students/:studentId', async (req, res) => {
+//     try {
+//         const studentId = req.params.studentId;
+//         await db.query('DELETE FROM students WHERE id=$1', [studentId]);
+//         console.log("From the delete request-url", studentId);
+//         res.status(200).end();
+//     } catch (e) {
+//         console.log(e);
+//         return res.status(400).json({ e });
 
-    }
-});
+//     }
+// });
 
-//A put request - Update a student 
-app.put('/api/students/:studentId', async (req, res) =>{
+//A put request - Update a contact 
+app.put('/api/contactlist/:contactId', async (req, res) =>{
     //console.log(req.params);
-    //This will be the id that I want to find in the DB - the student to be updated
-    const studentId = req.params.studentId
-    const updatedStudent = { id: req.body.id, firstname: req.body.firstname, lastname: req.body.lastname, iscurrent: req.body.is_current}
-    console.log("In the server from the url - the student id", studentId);
-    console.log("In the server, from the react - the student to be edited", updatedStudent);
+    //This will be the id that I want to find in the DB - the contact to be updated
+    const contactId = req.params.contactId
+    const updatedContact = { id: req.body.id, name: req.body.name, email: req.body.email, phone: req.body.phone, notes: req.body.notes }
+    console.log("In the server from the url - the contact id", contactId);
+    console.log("In the server, from the react - the contact to be edited", updatedContact);
     // UPDATE students SET lastname = "something" WHERE id="16";
-    const query = `UPDATE students SET firstname=$1, lastname=$2, is_current=$3 WHERE id=${studentId} RETURNING *`;
-    const values = [updatedStudent.firstname, updatedStudent.lastname, updatedStudent.iscurrent];
+    const query = `UPDATE contact SET name=$1, email=$2, phone=$3, notes=$4 WHERE id=${contactId} RETURNING *`;
+    const values = [updatedContact.name, updatedContact.email, updatedContact.phone, updatedContact.notes];
     try {
       const updated = await db.query(query, values);
       console.log(updated.rows[0]);
