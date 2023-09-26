1c9ef826d8f2b0130f1f5b4a9ed623f36f6acd9b
Janet Jiang <janetjiang1103@gmail.com>
2023-09-26 22:38:58 +0800

    create a form to get contact info from user

--- client/src/components/CreateContact.jsx
+++ client/src/components/CreateContact.jsx
@@ -1,21 +1,21 @@
-import React, { useState, useEffect } from 'react'
-import { Button, Form } from "react-bootstrap"
+import { useState, useEffect } from "react";
+import { Button, Form } from "react-bootstrap";
 
-const MyForm = ({ onSaveContact, editingContact, onUpdateContact }) => {
-
-    // This is the original State with not initial student 
-    const [contact, setContact] = useState(editingContact || {
-        name: "",
-        email: "",
-        phone: "",
-        notes: "",
-    });
+const createContact = ({ onSaveContact, editingContact, onUpdateContact }) => {
+    // This is the original State with not initial student
+    const [contact, setContact] = useState(
+        editingContact || {
+            name: "",
+            email: "",
+            phone: "",
+            notes: "",
+        }
+    );
 
     //create functions that handle the event of the user typing into the form
     const handleNameChange = (event) => {
         const name = event.target.value;
         setContact((contact) => ({ ...contact, name }));
-
     };
 
     const handleEmailChange = (event) => {
@@ -25,19 +25,26 @@ const MyForm = ({ onSaveContact, editingContact, onUpdateContact }) => {
 
     const handlePhoneChange = (event) => {
         const phone = event.target.value;
+        //console.log(iscurrent);
         setContact((contact) => ({ ...contact, phone }));
     };
 
+    const handleNotesChange = (event) => {
+        const notes = event.target.value;
+        //console.log(iscurrent);
+        setContact((contact) => ({ ...contact, notes }));
+    };
+
     const clearForm = () => {
-        setContact({ name: "", email: "", phone: "" })
-    }
+        setContact({ name: "", email: "", phone: "", notes: "" });
+    };
 
     //A function to handle the post request
     const postContact = (newContact) => {
         return fetch("http://localhost:8080/api/contactlist", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(newContact),
+            body: JSON.stringify(newContact)
         })
             .then((response) => {
                 return response.json();
@@ -51,12 +58,12 @@ const MyForm = ({ onSaveContact, editingContact, onUpdateContact }) => {
             });
     };
 
-    //A function to handle the put request
+    //A function to handle the post request
     const putContact = (toEditContact) => {
         return fetch(`http://localhost:8080/api/contactlist/${toEditContact.id}`, {
             method: "PUT",
             headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(toEditContact),
+            body: JSON.stringify(toEditContact)
         })
             .then((response) => {
                 return response.json();
@@ -67,7 +74,6 @@ const MyForm = ({ onSaveContact, editingContact, onUpdateContact }) => {
                 clearForm();
             });
     };
-
 
     //A function to handle the submit in both cases - Post and Put request!
     const handleSubmit = (e) => {
@@ -80,7 +86,7 @@ const MyForm = ({ onSaveContact, editingContact, onUpdateContact }) => {
     };
 
     return (
-        <Form className='form-students' onSubmit={handleSubmit}>
+        <Form className="form-students" onSubmit={handleSubmit}>
             <Form.Group>
                 <Form.Label>Name</Form.Label>
                 <input
@@ -103,7 +109,8 @@ const MyForm = ({ onSaveContact, editingContact, onUpdateContact }) => {
                     onChange={handleEmailChange}
                 />
             </Form.Group>
-            <Form.Label>Phone</Form.Label>
+            <Form.Group>
+                <Form.Label>Phone</Form.Label>
                 <input
                     type="text"
                     id="add-user-lastname"
@@ -112,13 +119,27 @@ const MyForm = ({ onSaveContact, editingContact, onUpdateContact }) => {
                     value={contact.phone}
                     onChange={handlePhoneChange}
                 />
+            </Form.Group>
+            <Form.Label>Notes</Form.Label>
+            <textarea
+                placeholder="Enter your note"
+                value={contact.notes}
+                onChange={handleNotesChange}
+                rows={4} // You can adjust the number of rows as needed
+            ></textarea>
+
             <Form.Group>
-            <Button type="submit" variant="outline-success">{contact.id ? "Edit Contact" : "Add Contact"}</Button>
-            {contact.id ? <Button type="button" variant="outline-warning" onClick={clearForm}>Cancel</Button> : null}
+                <Button type="submit" variant="outline-success">
+                    {contact.id ? "Edit Contact" : "Add Contact"}
+                </Button>
+                {contact.id ? (
+                    <Button type="button" variant="outline-warning" onClick={clearForm}>
+                        Cancel
+                    </Button>
+                ) : null}
             </Form.Group>
         </Form>
     );
 };
 
-
-export default MyForm
+export default createContact;
